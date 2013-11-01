9b48a69d97d65de9a746a34cf9b1e610a5b56d10
Jake Hendley <jake.hendley25@gmail.com>
2013-11-01 14:32:37 -0400

    added jquery.webticker.js, continued work on fullscreen

--- app/scripts/misc/junk-drawer.js
+++ app/scripts/misc/junk-drawer.js
@@ -4,3 +4,12 @@
       <p style="color: <%= repo.get('gitHUDMeta').donutData.color %>;"><%= contributor.login %> <%= repo.get('gitHUDMeta').commits[i] %></p>
     <% }) %>
   </div>
+
+
+  //each loop for contributors
+
+  <h6><span class="muted-text">Top Contributer: </span><%= repo.get('gitHUDMeta').tickerData.topCommiter.allTime.user %></h6>
+        <h3>misc data:</h3>
+            <% _.each(repo.get('gitHUDMeta').tickerData.allCommiters.allTime, function(x){ %>
+                <span><%=  x.user %>, <%= x.commits  %></span>
+            <% }) %>
