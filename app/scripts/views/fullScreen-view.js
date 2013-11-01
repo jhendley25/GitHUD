15977712d45a98c9fa186dede5b9da875c8504f9
Jake Hendley <jake.hendley25@gmail.com>
2013-11-01 15:04:52 -0400

    that there webticker()'s a pain in the ass

--- app/scripts/views/fullScreen-view.js
+++ app/scripts/views/fullScreen-view.js
@@ -11,8 +11,9 @@ GitHUD.Views.FullScreenView = Backbone.View.extend({
     // get the this.el into the page
     $(".fullscreen-stage").append(this.el)
     this.render()
+    this.initWebTicker()
     // listen for this view's model to change, then render
-    
+
   },
 
   render: function() {
@@ -35,5 +36,21 @@ GitHUD.Views.FullScreenView = Backbone.View.extend({
 
   exitFullscreen: function(){
     this.$el.remove()
+  },
+
+  initWebTicker: function(){
+
+    $("#webticker").webTicker({
+        speed: 50, //pixels per second
+        direction: "left", //if to move left or right
+        moving: true, //weather to start the ticker in a moving or static position
+        startEmpty: true, //weather to start with an empty or pre-filled ticker
+        duplicate: false, //if there is less items then visible on the ticker you can duplicate the items to make it continuous
+        rssurl: false, //only set if you want to get data from rss
+        rssfrequency: 0, //the frequency of updates in minutes. 0 means do not refresh
+        updatetype: "reset" //how the update would occur options are "reset" or "swap"
+    });
+
+
   }
 })
