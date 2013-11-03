373b1a73a08b15cecbdedb14d4c5d791cede0f7d
Jake Hendley <jake.hendley25@gmail.com>
2013-11-03 18:10:51 -0500

    prettified and added scale logic to fullscreen-view

--- app/scripts/views/fullScreen-view.js
+++ app/scripts/views/fullScreen-view.js
@@ -38,8 +38,29 @@ GitHUD.Views.FullScreenView = Backbone.View.extend({
     // console.log('githubmeta',this.model.get('gitHUDMeta').graphData)
     var ctx2 = $("#line-chart-" + this.model.get('id')).get(0).getContext("2d");
 
-    new Chart(ctx2).Line(this.model.get('gitHUDMeta').graphData);
-    // console.log(this.model)
+    var maxCommits = _.max(this.model.get('gitHUDMeta').graphData.truncatedWeeklyData.datasets[0].data, function(x){ return x })
+    if ( maxCommits > 9) {
+      // let the scale be automatically calculated if at least one of the data points is 10+
+      new Chart(ctx2).Line(this.model.get('gitHUDMeta').graphData.truncatedWeeklyData, {
+        scaleShowGridLines : false,
+        animation: false,
+        scaleSteps : 5,
+        scaleStepWidth : Math.floor(maxCommits / 5),
+        scaleStartValue : 0,
+        scaleOverride : true,
+        animation: false
+      })
+    } else {
+      // manually specify the scale because none of the data points were over 9
+      new Chart(ctx2).Line(this.model.get('gitHUDMeta').graphData.truncatedWeeklyData, {
+        scaleShowGridLines : false,
+        scaleSteps : 5,
+        scaleStepWidth : 2,
+        scaleStartValue : 0,
+        scaleOverride : true,
+        animation: false
+      });
+    }
   },
 
   exitFullscreen: function(){
