9b515d8882896bfa6ed64b5a39daa0fe443cd601
jhendley25 <jake.hendley25@gmail.com>
2013-11-10 17:13:45 -0500

    added unfinished integration testing

---
+++ test/spec/integrationTest.js
@@ -0,0 +1,10 @@
+describe("Integration testing", function(){
+  describe("Backbone Params-based navigation", function(){
+    it("true = true for now", function(done){
+      expect('awesome').not.to.equal(true)
+      done()
+
+    })
+    
+  })
+})
