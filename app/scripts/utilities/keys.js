000b08f8af782606bb5651e2829ace7509fb9d08
jhendley25 <jake.hendley25@gmail.com>
2013-11-12 11:35:41 -0500

    added responsive stuff to top menu

--- app/scripts/utilities/keys.js
+++ app/scripts/utilities/keys.js
@@ -6,7 +6,7 @@ GitHUD.utilities.keys = function(){
 GitHUD.utilities.colorTable = {};
 
 GitHUD.utilities.color = function(username){
-    // var pallete = ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"] 
+    // var pallete = ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"]
 
     var pallete = [
       '#B82424', // red
@@ -34,6 +34,7 @@ GitHUD.utilities.colorLuminance = function (hex, lum) {
     hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
   }
   lum = lum || 0;
+
 
   // convert to decimal and change luminosity
   var rgb = "#", c, i;
