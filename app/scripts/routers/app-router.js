f628ccaa5748d728c44985377bc6bf5e97d42d2f
jhendley25 <jake.hendley25@gmail.com>
2013-11-14 10:15:25 -0500

    extended life of auth cookie

--- app/scripts/routers/app-router.js
+++ app/scripts/routers/app-router.js
@@ -26,7 +26,7 @@ AppRouter = Backbone.Router.extend({
       //after the user clicks login and returns from github auth steps
       if (params && params.code){
         $.getJSON('http://githud-auth.herokuapp.com/authenticate/'+params.code, function(data) {
-          docCookies.setItem("access_token", data.token)
+          docCookies.setItem("access_token", data.token, new Date(2020, 5, 12))
           GitHUD.access_token = docCookies.getItem("access_token");
           $('.login').hide()
           $('.logout').show()
