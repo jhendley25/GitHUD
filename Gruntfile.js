a9b71b3d9bb53929ddfd0d4e6011bca6bde0376b
jhendley25 <jake.hendley25@gmail.com>
2014-02-01 15:55:04 -0500

    first build.  everything errored

--- Gruntfile.js
+++ Gruntfile.js
@@ -381,15 +381,9 @@ module.exports = function (grunt) {
 
     grunt.registerTask('build', [
         'clean:dist',
-        'useminPrepare',
         'concurrent:dist',
         'autoprefixer',
-        'concat',
-        'cssmin',
-        'uglify',
-        'copy:dist',
-        'rev',
-        'usemin'
+        'copy:dist'
     ]);
 
     grunt.registerTask('default', [
