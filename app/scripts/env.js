790c401a314dca7b1b229552936629b1e443c95f
JMaxwell <maxwell.joshua@gmail.com>
2014-03-11 19:33:32 -0400

    added my client ID

--- app/scripts/env.js
+++ app/scripts/env.js
@@ -1,6 +1,6 @@
-ENV = {
-	gitHubClientId: '0f4b65c00e68a20b1b2e'
-}
+// ENV = {
+// 	gitHubClientId: '0f4b65c00e68a20b1b2e'
+// }
 
 //overwriting mason's app client ID so I can change the redirect url inside github application settings
 ENV = {
